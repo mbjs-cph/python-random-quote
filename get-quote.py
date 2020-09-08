9d7064375d77e9bfc648da9abf63bc0f6045e222
Bilal Jamil <B000559@prod.sitad.dk>
2020-09-08 11:40:27 +0200

    Random quote bot is working with ekstraline removed

--- get-quote.py
+++ get-quote.py
@@ -6,9 +6,17 @@ def simple():
   quotes = f.readlines()
   f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  last = 15
+  rnd1 = random.randint(0, last)
+  rnd2 = random.randint(0, last)
+  #print(quotes[rnd1])
+  #print(quotes[rnd2])
+  print(quotes[rnd1], end = '')
+  print(quotes[rnd2])
+ 
+  
+  
+  
 
 if __name__== "__main__":
   simple()
